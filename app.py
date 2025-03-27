1edd10d4fc6e8c94ece85ad95ea7f108431af83f
Wentzel Oscar <wentzelo@cubbli24-gold.cs.helsinki.fi>
2025-03-27 16:05:47 +0200

    Added ability to write reviews

--- app.py
+++ app.py
@@ -12,6 +12,16 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_post")
+def new_post():
+    return render_template("new_post.html")
+
+@app.route("/create_post", methods=["POST"])
+def create_post():
+    title = request.form["title"]
+    rating = request.form["rating"]
+    review_text = request.form["review_text"]
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -45,10 +55,13 @@ def login():
     username = request.form["username"]
     password = request.form["password"]
     
-    sql = "SELECT password_hash FROM users WHERE username = ?"
-    password_hash = db.query(sql, [username])[0][0]
+    sql = "SELECT id, password_hash FROM users WHERE username = ?"
+    result = db.query(sql, [username])[0]
+    user_id = result["id"]
+    password_hash = result["password_hash"]
 
     if check_password_hash(password_hash, password):
+        session["user_id"] = user_id
         session["username"] = username
         return redirect("/")
     else:
@@ -56,5 +69,6 @@ def login():
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -3,3 +3,12 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    review TEXT,
+    rating INTEGER CHECK (rating >= 1 AND rating <= 5),
+    user_id INTEGER REFERENCES user
+);
