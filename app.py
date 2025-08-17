a9a542ba31ed236e8194c164e15399a487a4b869
wentzelo <oscar.g.wentzel@gmail.com>
2025-08-17 17:46:40 +0300

    Improve UX and validation for posts and comments. Preserve form data in session on validation errors (new_post, create_post). Added better validation and error handling.

--- app.py
+++ app.py
@@ -14,11 +14,13 @@ import users
 app = Flask(__name__)
 app.secret_key = config.secret_key
 
+
 @app.before_request
 def before_request():
     g.start_time = time.time()
     if "csrf_token" not in session:
         session["csrf_token"] = secrets.token_hex(16)
+
 
 @app.after_request
 def after_request(response):
@@ -26,19 +28,23 @@ def after_request(response):
     print("elapsed time:", elapsed_time, "s")
     return response
 
+
 def check_login():
     if "user_id" not in session:
         abort(403)
 
+
 def check_csrf():
     if request.form["csrf_token"] != session["csrf_token"]:
         abort(403)
+
 
 @app.template_filter()
 def show_lines(content):
     content = str(markupsafe.escape(content))
     content = content.replace("\n", "<br />")
     return markupsafe.Markup(content)
+
 
 @app.route("/")
 @app.route("/page/<int:page>")
@@ -56,6 +62,7 @@ def index(page=1):
     page_posts = posts.get_posts(page, page_size)
     return render_template("index.html", posts=page_posts, page=page, page_count=page_count)
 
+
 @app.route("/post/<int:post_id>")
 def show_post(post_id):
     post = posts.get_post(post_id)
@@ -65,15 +72,18 @@ def show_post(post_id):
 
     comments = posts.get_comments(post_id)
 
-    return render_template("show_post.html",
-                                            post=post,
-                                            genres=genres,
-                                            comments=comments)
+    return render_template("show_post.html", post=post, genres=genres, comments=comments)
+
 
 @app.route("/new_post")
 def new_post():
     check_login()
-    return render_template("new_post.html")
+    # If there is form data in the session, it will be used to pre-fill the form
+    form_data = session.get("form_data", {})
+    predefined_genres = ["Toiminta", "Draama", "Komedia", "Sci-fi", "Kauhu", "Romantiikka",
+                        "Dokumentti", "Seikkailu", "Fantasia", "Animaatio"]
+    return render_template("new_post.html", form_data=form_data, predefined_genres=predefined_genres)
+
 
 @app.route("/create_post", methods=["POST"])
 def create_post():
@@ -83,32 +93,51 @@ def create_post():
     today = date.today().isoformat()
 
     title = request.form["title"].strip()
+    rating = request.form["rating"]
+    review_text = request.form["review_text"]
+    watch_date = request.form["watch_date"]
+    genres = request.form.getlist("genres")
+    custom_genre = request.form.get("custom_genre", "").strip()
+
+    # Saves the information in case of an error
+    form_data = {
+        "title": title,
+        "rating": rating,
+        "review_text": review_text,
+        "watch_date": watch_date,
+        "genres": genres,
+        "custom_genre": custom_genre
+    }
+
     if not title:
         flash("VIRHE: Elokuvan nimi ei voi olla tyhjä tai pelkkä välilyönti")
+        session["form_data"] = form_data
         return redirect("/new_post")
     if len(title) > 100:
         flash("VIRHE: Elokuvan nimi on liian pitkä (max 100 merkkiä)")
+        session["form_data"] = form_data
         return redirect("/new_post")
 
-    rating = request.form["rating"]
-    review_text = request.form["review_text"]
     if len(review_text) > 4200:
         flash("VIRHE: Arvostelu on liian pitkä (max 4200 merkkiä)")
+        session["form_data"] = form_data
         return redirect("/new_post")
 
-    watch_date = request.form["watch_date"]
     if watch_date > today:
         flash("VIRHE: Katsomispäivä ei voi olla tulevaisuudessa.")
+        session["form_data"] = form_data
         return redirect("/new_post")
 
     user_id = session["user_id"]
-    genres = request.form.getlist("genres")
-    custom_genre = request.form.get("custom_genre", "").strip()
 
     post_id = posts.add_post(title, rating, review_text, watch_date, user_id)
     posts.update_post_genres(post_id, genres, custom_genre)
 
+    # Deletes the info after successful post creation
+    session.pop("form_data", None)
+
     return redirect("/")
+
 
 @app.route("/edit_post/<int:post_id>", methods=["GET", "POST"])
 def edit_post(post_id):
@@ -136,24 +165,38 @@ def edit_post(post_id):
 
     # POST
     check_csrf()
-    title = request.form["title"]
+    
+    today = date.today().isoformat()
+    
+    title = request.form["title"].strip()
+    
+    if not title:
+        flash("VIRHE: Elokuvan nimi ei voi olla tyhjä")
+        return redirect(f"/edit_post/{post_id}")
     if len(title) > 100:
-        abort(403)
+        flash("VIRHE: Elokuvan nimi on liian pitkä (max 100 merkkiä)")
+        return redirect(f"/edit_post/{post_id}")
 
     rating = request.form["rating"]
     review_text = request.form["review_text"]
+    
     if len(review_text) > 4200:
-        abort(403)
+        flash("VIRHE: Arvostelu on liian pitkä (max 4200 merkkiä)")
+        return redirect(f"/edit_post/{post_id}")
 
     watch_date = request.form["watch_date"]
+    if watch_date > today:
+        flash("VIRHE: Katsomispäivä ei voi olla tulevaisuudessa.")
+        return redirect(f"/edit_post/{post_id}")
+        
     selected_genres = request.form.getlist("genres")
     custom_genre = request.form.get("custom_genre", "").strip()
 
     posts.update_post(post_id, title, rating, review_text, watch_date)
-
     posts.update_post_genres(post_id, selected_genres, custom_genre)
 
     return redirect(f"/post/{post_id}")
+
 
 @app.route("/remove_post/<int:post_id>", methods=["GET", "POST"])
 def remove_post(post_id):
@@ -173,11 +216,13 @@ def remove_post(post_id):
         posts.delete_post(post_id)
     return redirect("/")
 
+
 @app.route("/search_post")
 def search_post():
     query = request.args.get("query")
     results = posts.find_posts(query) if query else []
     return render_template("search_post.html", query=query, results=results)
+
 
 @app.route("/user/<int:user_id>")
 def show_user(user_id):
@@ -187,9 +232,14 @@ def show_user(user_id):
     user_posts = users.get_user_posts(user_id)
     return render_template("show_user.html", user=user, posts=user_posts)
 
+
 @app.route("/comment/<int:post_id>", methods=["POST"])
 def give_comment(post_id):
     check_csrf()
+
+    post = posts.get_post(post_id)
+    if not post:
+        abort(404)
 
     reaction = request.form.get("reaction")
     text = request.form.get("comment", "").strip()
@@ -197,14 +247,20 @@ def give_comment(post_id):
     if reaction not in ("good", "bad"):
         abort(400)
 
+    if len(text) > 1000:
+        flash("VIRHE: Kommentti on liian pitkä (max 1000 merkkiä)")
+        return redirect(f"/post/{post_id}")
+
     is_positive = 1 if reaction == "good" else 0
     posts.add_comment(post_id, is_positive, text)
 
     return redirect(f"/post/{post_id}")
 
+
 @app.route("/register")
 def register():
     return render_template("register.html")
+
 
 @app.route("/create", methods=["POST"])
 def create():
@@ -233,6 +289,7 @@ def create():
 
     return render_template("register_return.html")
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
@@ -256,6 +313,7 @@ def login():
 
     flash("VIRHE: Väärä tunnus tai salasana")
     return redirect("/login")
+
 
 @app.route("/logout")
 def logout():
